1bb6e1391e13d303f9c4fcd13539782cede5c6df
Dino Stojkoski <stojkoskidino@gmail.com>
2024-11-16 01:00:23 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // Enable class-based dark mode
+  content: [
+    "./index.html",        // Include the HTML file
+    "./assets/**/*.css",   // Include CSS files if needed
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+};
+
+
